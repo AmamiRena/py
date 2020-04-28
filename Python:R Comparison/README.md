e4d09eb82af9dbaa6eb8cfc52c410a0ddba65394
Rena <mayuzumi159@gmail.com>
2020-04-28 17:47:10 -0500

    Tuesday

--- Python:R Comparison/README.md
+++ Python:R Comparison/README.md
@@ -1,10 +1,16 @@
 ## Data Analysis between Python and R
 ```
 .
-└── 1
-    ├── MSHA_Mine_Data_2013-2016.csv
-    ├── Mining\ Project.Rmd
-    ├── Mining\ Project.html
-    ├── Mining\ Project.ipynb
-    └── README.md
+├── 1
+│   ├── MSHA_Mine_Data_2013-2016.csv
+│   ├── Mining\ Project.Rmd
+│   ├── Mining\ Project.html
+│   ├── Mining\ Project.ipynb
+│   └── README.md
+├── 2
+│   ├── 2019-06-13-exam-pa-data-file.csv
+│   ├── Cars.Rmd
+│   ├── Cars.html
+│   └── README.md
+└── README.md
 ```
--- txt解析/handler.py
+++ txt解析/handler.py
@@ -18,3 +18,60 @@ class Handler:
                 result=match.group(0)
             return result
         return subs
+    
+
+class HTMLRenderer(Handler):
+    '''
+    add html
+    '''
+    def start_document(self):
+        print('<html><head><title>txt read</title></head><body>')
+    
+    def end_document(self):
+        print('</body></html>')
+    
+    def start_paragraph(self):
+        print('<p style="color: #444;">')
+    
+    def end_paragraph(self):
+        print('</p>')
+    
+    def start_heading(self):
+        print('<h2 style="color: #68BE5D;">')
+    
+    def end_heading(self):
+        print('</h2>')
+
+    def start_list(self):
+        print('<ul style="color: #363736;">')
+    
+    def end_list(self):
+        print('</ul>')
+    
+    def start_listitem(self):
+        print('<li>')
+    
+    def end_listitem(self):
+        print('</li>')
+    
+    def start_title(self):
+        print('<h1 style="color: #1ABC9C;">')
+    
+    def end_title(self):
+        print('</h1>')
+    
+    def sub_emphasis(self, match):
+        return('<em>%s</em>' % match.group(1))
+    
+    def sub_url(self, match):
+        s = ('<a target="_blank" style="text-decoration: none;'
+                'color: #BC1A4B;" href="{}">{}</a>')
+        return s.format(match.group(1), match.group(1))
+    
+    def sub_mail(self, match):
+        s = ('<a style="text-decoration: none;color: #BC1A4B;" '
+                'href="mailto:{}">{}</a>')
+        return s.format(match.group(1), match.group(1))
+    
+    def feed(self, data):
+        print(data)
